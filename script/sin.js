535586ec2d2236568033f771a2be065a803f52b7
Alexey Karpov <karpovad@yandex.ru>
2015-01-27 22:43:59 +0300

    sin(1/x), улучшения

--- script/sin.js
+++ script/sin.js
@@ -1,22 +1,34 @@
 (function () {
-    var plotter = new Plotter("plot");
+    var plotter = new Plotter("plot", {
+        left: -0.4,
+        right: 0.4,
+        top: 1.2,
+        bottom: -1.2
+    });
 
-    var sin = function (x) {
-        return Math.sin(1 / x);
+    var style = {
+        strokeWidth: 1
     };
 
-    plotter.addFunc(sin, {
-        right: -0.2
-    });
+    var leftSeparator = 0.2;
+    var rightSeparator = -0.2;
 
-    plotter.addFunc(sin, {
-        left: 0.2
-    });
+    var sin = function (x) {
+        return Math.sin(1/x);
+    };
 
-    plotter.addFunc(sin, {
-        left: -0.2,
-        right: 0.2,
-        accuracy: 5000,
+    plotter.addFunc(sin, _.extend(_.clone(style), {
+        right: rightSeparator
+    }));
+
+    plotter.addFunc(sin, _.extend(_.clone(style), {
+        left: leftSeparator
+    }));
+
+    plotter.addFunc(sin, _.extend(_.clone(style), {
+        left: rightSeparator,
+        right: leftSeparator,
+        accuracy: 20000,
         breaks: [0]
-    })
+    }));
 }());
